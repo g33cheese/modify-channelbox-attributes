03bcfabcb5e0b698507aae77ea42b62cbf39b3b7
Garvey <garveychi.gmail.com>
2025-01-23 14:26:37 -0500

    Changed the tool we are making. Removed old tool files.

---
+++ 0_app/importDeformerWeights.py
@@ -0,0 +1,14 @@
+#*********************************************************************
+# content	= Import specified deformer weights
+# version	= 1.0.0
+# date		= 2025-01-25
+# 
+# author	= Garvey Chi
+#*********************************************************************
+
+from maya import cmds
+import os  
+
+
+def importDeformerWeights():
+	pass
