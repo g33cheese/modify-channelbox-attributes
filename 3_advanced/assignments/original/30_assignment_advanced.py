2801377daff38f43aafcbe72ad373fd9f2404bb6
Garvey <garveychi.gmail.com>
2025-02-17 13:38:38 -0500

    INIT 3_advanced

---
+++ 3_advanced/assignments/original/30_assignment_advanced.py
@@ -0,0 +1,71 @@
+# ADVANCED **************************************************************************
+# content = assignment (Python Advanced)
+#
+# deliver = Upload the files to your git repository
+#           and share it in the assignment submission form.
+#
+# date    = 2022-08-07
+# email   = contact@alexanderrichtertd.com
+#*******************************************************************************
+"""
+
+Advanced Python is about a higher and more structural level of Python.
+Learning new functions, constructs and a better way of collaboration.
+
+
+
+#*******************************************************************************
+# 31. CUBE CLASS
+#*******************************************************************************
+Create a cube class in cube.py
+
+
+
+
+
+
+
+#*******************************************************************************
+# 32. DECORATOR
+#*******************************************************************************
+   a) Create a decorator in decorator.py
+
+   b) Include a decorator into your own application.
+
+
+
+
+
+#*******************************************************************************
+# 33. CLASSES & PARENTING
+#*******************************************************************************
+
+   a) Commit the "old" application version as "INIT 3_advanced"
+
+   b) Update one of your modules using classes.
+      Make sure to use one of the benefits of classes (else it's pointless).
+      Write a comment in the docString of the class why a class would improve this module.
+
+   c) OPTIONAL: Update one of your modules using class parenting.
+      Write a comment in the docString of the class why parenting would improve this module.
+
+      NOTE: c) & d) can be combined.
+
+
+
+
+
+
+
+#*******************************************************************************
+# 34. CONFIGURATION FILES
+#*******************************************************************************
+
+   a) Update one of your modules using configuration or data files.
+
+   b) Write a comment in the call of the configuration file
+      why a configuration or data file would improve this module.
+
+   c) Commit as "DONE 3_advanced" and push the updated version to "main/master".
+
+"""
