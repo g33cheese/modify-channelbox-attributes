00585bba12bb6d25571e2ff0b94fc188775cc13b
Garvey <garveychi.gmail.com>
2025-02-27 22:54:38 -0500

    DONE 41_simpleUI.py

---
+++ 4_ui/assignments/40_assignment_ui.py
@@ -0,0 +1,128 @@
+# UI ***************************************************************************
+# content = assignment
+#
+# deliver = Upload the files to your git repository
+#           and share it in the assignment submission form.
+#
+# date    = 2023-05-07
+# email   = contact@alexanderrichtertd.com
+#*******************************************************************************
+"""
+
+
+
+
+#*******************************************************************************
+# 40. PREPARE
+#*******************************************************************************
+    • Install PySide2
+    • Download and integrate Qt.py
+
+TIP: Check out pip!
+https://pypi.org/project/pip
+
+
+
+
+
+#*******************************************************************************
+# 41. RECREATE arUtil
+*******************************************************************************
+    • Check out the simpleUI.py module.
+
+    • Rebuild a simple UI by creating simpleUI.ui in Designer and connect it to simpleUI.py.
+
+    NOTE: The code in simpleUI.py is final.
+          Build the UI and take a screenshot.
+
+
+
+
+
+#*******************************************************************************
+# 43. REFERENCE app
+#*******************************************************************************
+
+Making your first UI using a reference.
+A great opportunity to play with all the different widgets and connections.
+
+    a) Choose an image reference of a tool (best to pick a DCC plugin)
+
+    b) Reconstruct the UI in the designer. Make sure to use layouts.
+
+    c) Add (real or pseudo) functionalities to the tool.
+
+
+NOTE: Combine this with the next task (04) if you don't need the extra practise.
+
+
+
+
+
+#*******************************************************************************
+# 44. CREATE UI
+#*******************************************************************************
+Create a UI for your application via code or designer.
+
+    a) Commit the "old" application version as "INIT 4_ui"
+
+        • Use PyQt5, PySide2 or Qt.py (whatever works best for you).
+        • Use buttons, checkboxes, sliders, edits, colors, layout … the more the better.
+        • Make sure that your UI is executable in OS and inside a DCC (e.g. Maya).
+        • Follow the UI design rules, make it pretty and make a screenshot.
+
+        • Record a one-minute-video (with or without voice over)
+          in which you show the app's functionalities in a real life scenario.
+          Make sure it "sells" your app to the audience aka "How does it help the production?"
+
+        • BONUS: Integrate the app into your DCC (shelf, toolbar, menu, ...)
+
+    b) Commit as "DONE 4_ui" and push the updated version to "main/master".
+
+
+
+
+
+#*******************************************************************************
+# 45. FINAL WORDS
+#*******************************************************************************
+
+    a) SHARE in the Discord/finals group:
+
+    * Which script editor will you use in the future and why?
+    [YOUR ANSWER]
+
+    * Which topic/part made the biggest impact to your application?
+    [YOUR ANSWER]
+
+    * What are your future plans regarding your application?
+    [YOUR ANSWER]
+
+    * Your repository link (if not private)
+    [YOUR ANSWER]
+
+    Share your tool with our Discord group at #finals:
+	1. Explain what your tool does and why
+	2. Share images and video links
+    
+
+    b) Watch the Final Words chapter after submitting the final assignment.
+
+
+    c) Fill out "Your Reflection" form:
+       https://forms.gle/qRuv27PmaWhEx7PB8
+
+
+
+
+#*******************************************************************************
+# 45. SHARING
+#*******************************************************************************
+Share your tool on LinkedIn as well and tag us: 
+
+   www.linkedin.com/in/alexanderrichtertd
+   www.linkedin.com/company/alexanderrichtertd
+
+   This is an opportunity to show your progress in Python
+   with your current and future employers.
+"""
---
+++ 4_ui/assignments/arSave.py
@@ -0,0 +1,136 @@
+# *********************************************************************
+# content   = arSave exercise
+# version   = 0.1.0
+# date      = 2025-02-27
+# 
+# author    = Garvey Chi garveychi@gmail.com
+# *********************************************************************
+
+
+import os
+import sys
+import webbrowser
+
+from Qt import QtWidgets, QtGui, QtCore, QtCompat
+
+# *********************************************************************
+# VARIABLES
+TITLE = os.path.splitext(os.path.basename(__file__))[0]
+CURRENT_PATH = os.path.dirname(__file__)
+IMG_PATH = CURRENT_PATH + "/img/{}.png"
+
+# *********************************************************************
+# CLASS
+
+
+class ArSave:
+    def __init__(self):
+        path_ui = CURRENT_PATH + '/' + TITLE + '.ui'
+        self.wgSave = QtCompat.loadUi(path_ui)
+
+        self.preview_img_path  = ''
+
+        self.wgSave.setWindowIcon(QtGui.QPixmap(IMG_PATH.format('btn_save')))
+
+        self.wgSave.btnPreview.setIcon(QtGui.QPixmap(IMG_PATH.format('default')))
+        self.wgSave.btnScreenshot.setIcon(QtGui.QPixmap(IMG_PATH.format('btn_camera')))
+        self.wgSave.btnRender.setIcon(QtGui.QPixmap(IMG_PATH.format('btn_render')))
+
+        self.wgSave.lblPen.setPixmap(QtGui.QPixmap(IMG_PATH.format('btn_write')))
+
+        self.wgSave.btnOpenFolder.setIcon(QtGui.QPixmap(IMG_PATH.format('btn_folder')))
+        self.wgSave.btnProject.setIcon(QtGui.QPixmap(IMG_PATH.format('btn_project')))
+        self.wgSave.btnUser.setIcon(QtGui.QPixmap(IMG_PATH.format('btn_user')))
+        self.wgSave.btnReport.setIcon(QtGui.QPixmap(IMG_PATH.format('btn_report')))
+        self.wgSave.btnHelp.setIcon(QtGui.QPixmap(IMG_PATH.format('btn_help')))
+
+        # SIGNAL
+        self.wgSave.btnSave.clicked.connect(self.press_btnSave)
+        self.wgSave.btnPublish.clicked.connect(self.press_btnPublish)
+        self.wgSave.btnVersionUp.clicked.connect(self.press_btnVersionUp)
+        self.wgSave.btnVersionDown.clicked.connect(self.press_btnVersionDown)
+
+        self.wgSave.btnPreview.clicked.connect(self.press_btnPublish)
+        self.wgSave.btnScreenshot.clicked.connect(self.press_btnScreenshot)
+        self.wgSave.btnRender.clicked.connect(self.press_btnRender)
+
+        self.wgSave.btnOpenFolder.clicked.connect(self.press_btnOpenFolder)
+        self.wgSave.btnUser.clicked.connect(self.press_btnUser)
+        self.wgSave.btnProject.clicked.connect(self.press_btnProject)
+        self.wgSave.btnReport.clicked.connect(self.press_btnReport)
+        self.wgSave.btnHelp.clicked.connect(self.press_btnHelp)
+
+        # SETUP
+        self.wgSave.editFile.setText('mike_RIG_v001')
+        self.wgSave.lblComment.setText('')
+        self.set_open_folder(CURRENT_PATH)
+
+        self.wgSave.show()
+
+
+    #*********************************************************************
+    # PRESS
+    def press_btnSave(self):
+        self.wgSave.lblComment.setText('Save file')
+
+    def press_btnPublish(self):
+        self.wgSave.lblComment.setText('Publish')
+
+    def press_btnVersionUp(self):
+        self.wgSave.lblComment.setText('Version Up')
+
+    def press_btnVersionDown(self):
+        self.wgSave.lblComment.setText('Version Down')
+
+    def press_btnOpenFolder(self):
+        webbrowser.open('self.open_path')
+
+    def press_btnUser(self):
+        webbrowser.open('https://www.alexanderrichtertd.com')
+
+    def press_btnProject(self):
+        webbrowser.open(self.open_path)
+
+    def press_btnReport(self):
+        webbrowser.open('https://github.com/alexanderrichtertd/plex/issues')
+
+    def press_btnHelp(self):
+        webbrowser.open('https://github.com/alexanderrichtertd/plex/wiki/arSave')
+
+    #*********************************************************************
+    # MENU
+    def press_btnRender(self):
+        if os.path.exists(self.preview_img_path):
+            webbrowser.open(os.path.realpath(self.preview_img_path))
+
+    def press_btnScreenshot(self):
+        self.wgSave.lblComment.setText('Create Screenshot')
+        screenshot_path = IMG_PATH.format('screenshot')
+        QtGui.QPixmap.grabWindow(QtWidgets.QApplication.desktop().winId()).save(screenshot_path, )
+        self.wgSave.btnPreview.setIcon(QtGui.QPixmap(screenshot_path))
+
+    def press_btnRender(self):
+        self.wgSave.lblComment.setText('Create Render')
+
+
+    #*********************************************************************
+    # FUNCTIONS
+    def set_open_folder(self, path=''):
+        ''' Sets the current folder path.
+
+        Args:
+            path (str, optional): folder path
+        '''
+        print(path)
+
+        if os.path.exists(path):
+            self.open_path = os.path.normpath(path)
+            self.wgSave.lblFolder.setText(self.open_path)
+
+
+# *********************************************************************
+# START UI
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    ar_save = ArSave()
+    app.exec_()
